d459584a08bed485065c1706c18b96bf91b09925
whatifif <whatifif@yahoo.com>
2016-01-31 18:12:54 +1100

    new post has a correct created date.

--- server/models/Post.js
+++ server/models/Post.js
@@ -10,16 +10,3 @@ var PostSchema = new mongoose.Schema({
 });
 
 var Post = module.exports = mongoose.model('Post', PostSchema);
-//--------------------------------------------------
-// var mongoose = require('mongoose');
-
-// var PostSchema = new mongoose.Schema({
-//     id: {type: String, required:true, unique:true},
-//     title: {type: String, required:true, unique:true},
-//     summary: {type: String},
-//     content: {type: String}, 
-//     created: {type: String},
-//     author: {type: mongoose.Schema.ObjectId, required:true, ref: 'User'}
-// });
-
-// var Post = module.exports = mongoose.model('Post', PostSchema);
