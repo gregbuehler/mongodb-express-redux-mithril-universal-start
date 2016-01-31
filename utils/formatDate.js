0b94d02f87ee59811f6e91f46ea45219a59a0c14
whatifif <whatifif@yahoo.com>
2016-01-31 19:30:11 +1100

    formateDate

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,7 +1,10 @@
-//format date as yyyy-mm-dd
+
 var formatDate = module.exports = function(dateString) {
         if (dateString) {
+            //format date as yyyy-mm-dd
             // return dateString.substring(0, 10);
+
+            //format date as yyyy-mm-dd hh:nn
             return dateString.substring(0, 16).replace('T', ' ');
         }
     }
