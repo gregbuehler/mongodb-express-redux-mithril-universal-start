d6a0a848d13e6d7af991c104e8f02e2f0251e897
whatifif <whatifif@yahoo.com>
2016-02-02 13:06:53 +1100

    rearrange

--- server/resources-old/blog/blogResource.js
+++
@@ -1,9 +0,0 @@
-// var Post = require('../../models/Post');
-var Post = require('../../pages/blog/postModel');
-
-var promise = Post.find().sort({
-    created: -1
-}).limit(10).select('-_id id title summary created author').populate('author', 'userid');
-
-module.exports = promise;
-
--- server/resources-old/user/usersResource.js
+++
@@ -1,9 +0,0 @@
-// var User = require('../../models/User');
-var User = require('../../user/userModel');
-
-var promise = User.find().sort({
-    userid: 1
-}).limit(10).select('-_id id userid email verified role');
-
-module.exports = promise;
-
