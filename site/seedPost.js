e2b1c4121cec26647c5d3ea1a4e2515775803232
whatifif <whatifif@yahoo.com>
2016-02-11 16:56:25 +1100

    seedPost improved.

--- site/seedPost.js
+++ site/seedPost.js
@@ -1,37 +1,38 @@
-/**
- * Populate DB with sample data on server start.
- * To disable, edit site/config/environment/development.js, and set `config.seedUser = false`
- */
 'use strict';
-// var postModel = require('../server/models/Post');
+
 var postModel = require('../server/pages/blog/postModel');
 var mockaroo = require('./mockPost');
-// var userModel = require('../server/models/User');
 var userModel = require('../server/pages/user/userModel');
 
 var seed = function() {
     // console.log('seedPost10-mockaroo', mockaroo);
-    var author = new userModel({
+    var user = new userModel({
         "id": 'zxcvbn',
         "userid": "author",
         "email": "author@author.com",
-        "password": "1111",
+        "password": "author",
         "verified": true,
-        "role": "member"
+        "role": "author"
     });
 
-    author.save();
+    userModel.create(user, function(err, author) {
+        if (err) {
+            return res.status(500).send({
+                errmsg: 'Author not created.'
+            })
+        }
 
-    mockaroo.forEach(function(post) {
-        post.id = post.title.replace(/\s/g, '_');
-        post.author = author._id;
-    })
 
-    postModel.find({}).remove(function() {
-        // console.log('this callback is needed for proper remove operation.')
-        postModel.create(mockaroo);
+        mockaroo.forEach(function(post) {
+            post.id = post.title.replace(/\s/g, '_');
+            post.author = author._id;
+        })
+
+        postModel.find({}).remove(function() {
+            // console.log('this callback is needed for proper remove operation.')
+            postModel.create(mockaroo);
+        });
     });
-
 
 }
 
--- site/seedUser.js
+++ site/seedUser.js
@@ -3,12 +3,13 @@
  * To disable, edit site/config/environment/development.js, and set `config.seedUser = false`
  */
 'use strict';
-// var userModel = require('../server/models/User');
+
 var userModel = require('../server/pages/user/userModel');
 
 var seed = function() {
 
     userModel.find({}).remove(function() {
+
         // console.log('this callback is needed for proper remove operation.')
         userModel.create([{
             "id": "qwerty",
